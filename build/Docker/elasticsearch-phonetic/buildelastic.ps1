27a1bbd406751e93fa99317cd9b9f63b4ca23479
Marcus Poulton <marcus.poulton@hotmail.com>
2017-07-02 18:26:10 +0100

    Docker config changes

--- build/Docker/elasticsearch-phonetic/buildelastic.ps1
+++ build/Docker/elasticsearch-phonetic/buildelastic.ps1
@@ -1,6 +1,3 @@
-docker build --tag=elasticsearch-phonetic .
-docker login -u gluteusmaximus -p siobhan89
-# tag the image with my account
-docker tag elasticsearch-phonetic gluteusmaximus/quest-elastic
+docker build --tag=gluteusmaximus/quest-elastic
 # push the image to docker
 docker push gluteusmaximus/quest-elastic
--- build/Docker/pushall.ps1
+++
@@ -1,6 +0,0 @@
-docker login -u gluteusmaximus -p siobhan89
-docker push gluteusmaximus/quest-mapserver:latest
-docker push gluteusmaximus/quest-database:latest
-docker push gluteusmaximus/quest-searchdatabase:latest
-docker push gluteusmaximus/quest.cmd:latest
-docker push gluteusmaximus/quest.web:latest
