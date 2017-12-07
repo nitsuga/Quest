b440337f95c47fc4cf18d72bc99f502e7f104a1e
Marcus Poulton <marcus.poulton@gmail.com>
2017-12-07 10:04:13 +0000

    ExpressQ additions

---
+++ src/Quest.ExpressQ/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Quest.ExpressQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
